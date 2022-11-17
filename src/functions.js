31ca85873e0335816585dfab7057cc82d2e393df
Kex1016 <nyilasdavid@gmail.com>
2022-11-17 21:14:47 +0100

    Initial commit

---
+++ src/functions.js
@@ -0,0 +1,194 @@
+function getAnilistID(username) {
+    return fetch('https://graphql.anilist.co', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+            query ($username: String) {
+              User(name: $username) {
+                id
+              }
+            }
+        `,
+            variables: {
+                username
+            }
+        })
+    })
+        .then(r => r.json())
+        .then(data => data.data.User)
+}
+
+function getAnilistUser(id) {
+    return fetch('https://graphql.anilist.co', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+            query ($id: Int) {
+              User(id: $id) {
+                name
+                avatar {
+                  large
+                }
+                bannerImage
+                statistics {
+                  anime {count}
+                  manga {count}
+                }
+                options {
+                  profileColor
+                }
+              }
+            }
+        `,
+            variables: {
+                id
+            }
+        })
+    })
+        .then(r => r.json())
+        .then(data => data.data.User)
+}
+
+const loadingHTML = `
+<div class="cakes_loading">
+    <div class="cakes_loading__dot"></div>
+    <div class="cakes_loading__dot"></div>
+    <div class="cakes_loading__dot"></div>
+</div>
+`
+
+const getCardContent = async (user) => {
+    return `<div class="cakes_card__avatar" style="background-image: url('${user.avatar.large}')"></div>
+    <div class="cakes_card__info">
+        <h1 class="cakes_card__info__name">${user.name}</h1>
+        <div class="cakes_card__info__stats">
+            <div class="cakes_card__info__stats__anime">
+                <h2>${user.statistics.anime.count}</h2>
+                <div>Anime</div>
+            </div>
+            <div class="cakes_card__info__stats__manga">
+                <h2>${user.statistics.manga.count}</h2>
+                <div>Manga</div>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function makeCard() {
+    const card = document.createElement('div')
+    card.classList.add('cakes_card')
+    return card;
+}
+
+async function retrieveUser(userid) {
+    let user = localStorage.getItem('cakes_user');
+    if (!user) {
+        user = {};
+        user[userid] = await getAnilistUser(userid);
+    } else {
+        user = JSON.parse(user);
+    }
+
+    if (user[userid]) {
+        let userObj = user[userid];
+        if (userObj.expires > Date.now()) {
+            return userObj;
+        }
+
+        // if expired, delete it
+        delete user[userid];
+
+        let seen = [];
+        localStorage.setItem('cakes_user', JSON.stringify(user, function (key, val) {
+            if (val != null && typeof val == "object") {
+                if (seen.indexOf(val) >= 0) {
+                    return;
+                }
+                seen.push(val);
+            }
+            return val;
+        }));
+    } else {
+        let userObj = await getAnilistUser(userid);
+        userObj.expires = Date.now() + 1000 * 60 * 60 * 0.5; // 30 minutes
+        user[userid] = userObj;
+
+        let seen = [];
+        localStorage.setItem('cakes_user', JSON.stringify(user, function (key, val) {
+            if (val != null && typeof val == "object") {
+                if (seen.indexOf(val) >= 0) {
+                    return;
+                }
+                seen.push(val);
+            }
+            return val;
+        }));
+
+        return userObj;
+    }
+}
+
+function renderCard(el, parent) {
+    // Give it a good old mouse enter
+    el.addEventListener("mouseenter", async (e) => {
+        // test if card is there
+        let uid;
+        if (el.innerText !== "") {
+            let username = el.innerText;
+            uid = (await getAnilistID(username)).id;
+        } else {
+            let url = el.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+            uid = url.split("/").pop().split("-")[0].slice(1);
+        }
+
+        const card = makeCard();
+        card.innerHTML = loadingHTML;
+        setTimeout(() => card.classList.add("cakes_card--show"), 100);
+        el.appendChild(card);
+
+        // if still hovered
+        if (el.matches(":hover")) {
+            let user = await retrieveUser(uid);
+            let cardContent = await getCardContent(user);
+            card.innerHTML = cardContent;
+            card.style.backgroundColor = user.options.profileColor;
+            card.style.backgroundImage = `url(${user.bannerImage})`;
+        }
+    });
+
+    // Give it a good old mouse leave
+    el.addEventListener("mouseleave", async (e) => {
+        const card = el.querySelector(".cakes_card");
+        if (card) {
+            card.classList.add("cakes_card--hide");
+            setTimeout(() => card.remove(), 300);
+        }
+    });
+}
+
+async function getUser(element) {
+    const classes = [ // The classes that should trigger the check for possible card placement
+        "activity-entry",
+        "thread-card",
+        "forum-thread"
+    ];
+    if (!element.classList) return;
+    if (classes.some(c => element.classList.contains(c))) {
+        // If the element has the class that should
+        // trigger the check for possible card placement
+        const avatar = element.querySelector(".avatar");
+        if (avatar) {
+            await renderCard(avatar, element);
+        }
+    }
+}
+
---
+++ src/template.m4
@@ -0,0 +1,45 @@
+m4_divert(-1)m4_dnl
+m4_changequote(<m4<,>m4>)
+m4_define(SCRIPT_VERSION, 0.6)
+m4_divert(0)m4_dnl
+// ==UserScript==
+// @name         AniList Profile Hover Cards
+// @namespace    https://anilist.co/user/cakes
+// @version      SCRIPT_VERSION
+// @description  Extra parts for Anilist.co
+// @author       cakes
+// @match        https://anilist.co/*
+// @grant        GM_xmlhttpRequest
+// @license      GPL-3.0-or-later
+// ==/UserScript==
+
+/*
+    Thanks for checking out my userscript!
+    Some elements might be familiar if you saw hoh's Automail userscript, as I've
+    borrowed small parts of it for this script. Credit where credit is due:
+    https://github.com/hohMiyazawa/Automail/
+    Check it out, it's an awesome script!
+*/
+
+(function(){
+"use strict";
+
+// Shared style element for all the modules. Custom classes are prefixed by "cakes" to avoid messing with AniList classes
+let style = document.createElement("style");
+style.id = "profile-card-styles";
+style.type = "text/css";
+
+style.textContent = `
+m4_include(css/style.css)
+`;
+
+let documentHead = document.querySelector("head");
+if(documentHead && document.URL !== "https://anilist.co/graphiql") documentHead.appendChild(style)
+else return
+
+// Load the modules
+m4_include(functions.js)
+m4_include(main.js)
+
+// End of script
+})();
